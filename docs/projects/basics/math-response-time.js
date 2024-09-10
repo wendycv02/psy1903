a5e575a780f03e486aac254e5ea522ea964e9a31
Wendy Carballo <wcarballovillanueva@college.harvard.edu>
2024-09-10 17:05:15 -0400

    Created response time experiment.

---
+++ docs/projects/basics/math-response-time.js
@@ -0,0 +1,12 @@
+alert("In this experiment we will measure your response time. You will be shown a series of simple math equations. Answer these equations as quickly and accurately as you can.");
+
+let response1 = prompt("What is 2 + 4?");
+alert("You answered " + response1);
+
+let response2 = prompt("What is 1 + 7?");
+alert("You answered " + response2);
+
+let response3 = prompt("What is 6 + 1?");
+alert("You answered " + response3);
+
+alert("Thank you for your participation!");
