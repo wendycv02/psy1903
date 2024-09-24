c0a2c7578037257597dda91e71263ae73076277b
Wendy Carballo <wcarballovillanueva@college.harvard.edu>
2024-09-24 17:26:51 -0400

    celciusToFahrenheit.

---
+++ docs/projects/week4-task-set/app.js
@@ -0,0 +1,12 @@
+
+// celciusToFahrenheit
+
+celsiusToFahrenheit();
+
+function celsiusToFahrenheit(celsius) {
+    let fahrenheit = (celsius * 1.8) + 32;
+    return fahrenheit;
+}
+
+console.log(celsiusToFahrenheit(10));
+console.log(celsiusToFahrenheit(-5));
