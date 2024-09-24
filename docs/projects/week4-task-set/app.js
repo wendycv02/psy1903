1682cd8c1e7096e0d2414b3e015722550d6531ee
Wendy Carballo <wcarballovillanueva@college.harvard.edu>
2024-09-24 17:42:29 -0400

    convertTemp

--- docs/projects/week4-task-set/app.js
+++ docs/projects/week4-task-set/app.js
@@ -1,12 +1,25 @@
 
 // celciusToFahrenheit
 
-celsiusToFahrenheit();
+// celsiusToFahrenheit();
 
-function celsiusToFahrenheit(celsius) {
-    let fahrenheit = (celsius * 1.8) + 32;
-    return fahrenheit;
+// function celsiusToFahrenheit(celsius) {
+//     let fahrenheit = (celsius * 1.8) + 32;
+//     return fahrenheit;
+// }
+
+// console.log(celsiusToFahrenheit(10));
+// console.log(celsiusToFahrenheit(-5));
+
+// convertTemp
+
+function convertTemp(temp, convertTo) {
+    if (convertTo == 'c') {
+        return ((temp - 32) / 1.8);
+    } else {
+        return ((temp * 1.8) + 32);
+    }
 }
 
-console.log(celsiusToFahrenheit(10));
-console.log(celsiusToFahrenheit(-5));
+console.log(convertTemp(10, 'c'));
+console.log(convertTemp(10, 'f'));
