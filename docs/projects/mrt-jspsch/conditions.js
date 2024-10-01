2485e95f75330b95d11948372d31f7bc1974bf3b
Wendy Carballo <wcarballovillanueva@college.harvard.edu>
2024-10-01 16:30:03 -0400

    Conditions.

---
+++ docs/projects/mrt-jspsch/conditions.js
@@ -0,0 +1,16 @@
+let conditions = [];
+
+for (i = 0; i < 3; i++) {
+    let num1 = getRandomNumber(10, 1);
+    let num2 = getRandomNumber(10, 1);
+    let condition = {
+        num1: num1,
+        num2: num2,
+        answer: num1 + num2
+    }
+    conditions.push(condition);
+}
+console.log(conditions);
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
