fe8133e6b4c29eaeb288df1b31a23e1f14182079
Wendy Carballo <wcarballovillanueva@college.harvard.edu>
2024-10-29 16:53:35 -0400

    .

--- docs/projects/plugin-demo/experiment.js
+++ docs/projects/plugin-demo/experiment.js
@@ -8,7 +8,7 @@ let welcomeTrial = {
     type: jsPsychHtmlKeyboardResponse,
     stimulus: `
     <h1 class='welcome'> Welcome to this experiment.</h1> 
-    <p>This experiment is a [insert plugin] demo.</p>
+    <p>This experiment is a [insert plugin] demo. You will be asked to categorize a series of faces on a trustworthiness dimension</p>
     <p>Press <span class='key'>SPACE</span> to begin.</p>
     `,
     choices: [' '],
@@ -16,4 +16,15 @@ let welcomeTrial = {
 
 timeline.push(welcomeTrial);
 
+let images = ["NeutralA.png", "NeutralB.png", "TrustworhtyA.png", "TrustworthyB.png", "UntrustworthyA.png", "UntrustworthyB.png"];
+
+let animationTrial = {
+    type: jsPsychAnimation,
+    stimuli: images,
+    sequence_reps: 3,
+    frame_time: 300,
+    prompt: '<p>Watch the faces.</p>',
+};
+
+timeline.push(animationTrial);
 jsPsych.run(timeline)
