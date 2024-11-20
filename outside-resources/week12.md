761ef36e659be9cabd0e5eac4647bafd2157ae44
Wendy Carballo <wcarballovillanueva@college.harvard.edu>
2024-11-20 14:41:21 -0500

    Week 12 Task Set

---
+++ outside-resources/week12.md
@@ -0,0 +1,9 @@
+# Outside Resources Log - Week 12 
+
+## AI Prompts
+* "Why is [insert For Loop] giving me this error: 'Error in [.data.frame(tmp, , tmp$rt) : undefined columns selected?'"
+* "Why am I getting this error: 'Error in [.data.frame(questionnaire, , rev_item) : 
+  undefined columns selected?'"
+* "What's going on with this loop? R seems to think it's unfinished code when I run it (given the + symbol in the console): [insert For Loop]"
+## Outside sites
+--None 
--- stats/rIntro/scripts/dataCleaningExercise.R
+++ stats/rIntro/scripts/dataCleaningExercise.R
@@ -18,7 +18,7 @@ library("ggplot2")
 
 #### (3) Read individual IAT data file --------
 
-iat_data1 <- read.csv("../../../osfstorage-archive/iat-2024-11-08-01-18-12.csv", header = TRUE, na.strings = "NA", stringsAsFactors = FALSE)
+iat_data1 <- read.csv("/Users/wendycarballo/Desktop/psy1903/osfstorage-archive /iat-2024-11-05-22-07-53.csv", header = TRUE, na.strings = "NA", stringsAsFactors = FALSE)
 str(iat_data1)
 summary(iat_data1)
 
@@ -30,9 +30,7 @@ iat_data2 <- iat_data1[iat_data1$expectedCategoryAsDisplayed == "healthy or fat"
                          iat_data1$expectedCategoryAsDisplayed == "unhealthy or fit",
                        c("trial_index", "rt", "response", "word", "expectedCategory", "expectedCategoryAsDisplayed", "leftCategory", "rightCategory", "correct")]
 
-
 #### (5) Refactoring ------------------
-
 str(iat_data2)
 summary(iat_data2)
 
@@ -50,32 +48,9 @@ for (column_name in column_names) {
 str(iat_data2)
 summary(iat_data2)
 
-
 #### (6) Creating a function ----------
-
-calculate_IAT_dscore <- function (tmp) {
-  # Step 2: Filter out trials with rt < 300 ms (subset full data frame into new data frame called tmp)
-  tmp <- iat_data2[iat_data2$rt > 300 & iat_data2$rt < 5000,]
-  # Step 3: Separate congruent and incongruent trials (subset tmp into two new data frames: congruent_trials and incongruent_trials) 
-  congruent_trials <- tmp[tmp$expectedCategoryAsDisplayed == "healthy or fit" |
-                            tmp$expectedCategoryAsDisplayed == "unhealthy or fat",]
-  incongruent_trials <- tmp[tmp$expectedCategoryAsDisplayed == "healthy or fat" |
-                              tmp$expectedCategoryAsDisplayed == "unhealthy or fit",]
-  # Step 4: Calculate mean for congruent and mean for incongruent trials (mean_congruent, mean_incongruent)
-  congruent_means <- mean(congruent_trials$rt, na.rm = TRUE)
-  incongruent_means <- mean(incongruent_trials$rt, na.rm = TRUE)
-  # Step 5: Calculate standard deviation for all trials (pooled_sd) 
-  pooled_sd <- sd(tmp$rt, na.rm = TRUE)
-  # Step 6: Calculate D-score
-  dscore <- (incongruent_means - congruent_means) / pooled_sd
-  # Step 7: Return D-score
-  return(dscore)
-}
-
-#### (7) For loop and new data files -------
-
 ## Set a variable called directory_path with the path to the location of your data csv files. This directory should *only* contain your raw participant csv data files and no other files.
-directory_path <- "~/Desktop/psy1903/osfstorage-archive"
+directory_path <- "~/Desktop/psy1903/osfstorage-archive "
 
 ## Create a list of all the files in that directory.
 files_list <- list.files(path = directory_path, pattern = "\\.csv$", full.names = TRUE)
@@ -92,34 +67,120 @@ head(dScores)
 colnames(dScores) <- c("participant_ID", "d_score")
 head(dScores)
 
+calculate_IAT_dscore <- function (data) {
+  # Step 2: Filter out trials with rt < 300 ms (subset full data frame into new data frame called tmp)
+  tmp <- data[data$rt > 300 & data$rt < 5000,]
+  # Step 3: Separate congruent and incongruent trials (subset tmp into two new data frames: congruent_trials and incongruent_trials) 
+  congruent_trials <- tmp[tmp$expectedCategoryAsDisplayed == "healthy or fit" |
+                            tmp$expectedCategoryAsDisplayed == "unhealthy or fat",]
+  incongruent_trials <- tmp[tmp$expectedCategoryAsDisplayed == "healthy or fat" |
+                              tmp$expectedCategoryAsDisplayed == "unhealthy or fit",]
+  # Step 4: Calculate mean for congruent and mean for incongruent trials (mean_congruent, mean_incongruent)
+  congruent_means <- mean(congruent_trials$rt, na.rm = TRUE)
+  incongruent_means <- mean(incongruent_trials$rt, na.rm = TRUE)
+  # Step 5: Calculate standard deviation for all trials (pooled_sd) 
+  pooled_sd <- sd(tmp$rt, na.rm = TRUE)
+  # Step 6: Calculate D-score
+  dscore <- (congruent_means - incongruent_means) / pooled_sd
+  # Step 7: Return D-score
+  return(dscore)
+}
+
+#### (7) For loop and new data files -------
+
 ## Initiate variable i to represent row numbers for each iteration, starting with 1
 i = 1
 
-## Now fill in the remaining code following the commented instructions:
+# Outside of the for loop, before saving, check the structure of the dScores data frame and, if necessary:
+# Change column "whichPrime" to be a factor (should have 2 or 3 levels depending on your prime).
+# Change "d_score" and "questionnaire" to be numbers
+# participant_ID can be character
 
 ## Initiate a for loop that iterates across each file in files_list
-
-# Set tmp file for testing 
-# file <- files_list[[1]]
-
 for (file in files_list) {
   # Use read.csv to read in your file as a temporary data frame called tmp
-  tmp <- read.csv(file, header = TRUE, stringsAsFactors = FALSE)
+  tmp <- read.csv(file)
+  tmp[tmp == ""] <- NA
   # Assign participant_ID as the basename of the file
   participant_ID <- tools::file_path_sans_ext(basename(file))
+  #   “rt” column is numeric
+  tmp$rt <- as.numeric(tmp$rt)
+  # “correct” column is logical
+  tmp$correct <- as.logical(tmp$correct)
+  # Appropriate columns are factors
+  columnNames <- c("expectedCategory", "expectedCategoryAsDisplayed", "leftCategory", "rightCategory")
+  for (columnName in columnNames) {
+    tmp[,columnName] <- as.factor(tmp[,columnName])
+  }
   # Isolate the participant_ID column for the current row number (i) and assign it to be the current participant_ID variable
   dScores[i, "participant_ID"] <- participant_ID
+  # Assign the dScores "whichPrime" column to be the current participant's prime label. 
+  dScores[i, "whichPrime"] <- tmp$displayedVideo[tmp$trialType == "prime" & tmp$displayedVideo != ""]
   # Using similar logic, isolate the d_score OR c("emotionA_d_score", "emotionB_d_score") column(s) for the current row number (i) and assign it to be the current d-score(s) by using our calculate_IAT_dscore or calculate_EST_dscore on the tmp data file
-  dScores[i, "d_score"] <- calculate_IAT_dscore()
+  dScores[i, "d_score"] <- calculate_IAT_dscore(tmp)
+  # Assign the "questionnaire" column to be the output of the score_questionnaire function.
+  dScores[i, "questionnaire"] <- score_questionnaire(tmp)
   # Remove the temporary data file tmp
   rm(tmp)
   # Increase our row number variable i by one for the next iteration
   i <- i + 1
 }
 
-View(dScores)
+str(dScores)
+dScores$whichPrime <- as.factor(dScores$whichPrime)
+str(dScores)
 
 ## Outside of the for loop, save the new dScores data frame using write.csv() into your data_cleaning/data subdirectory:
 write.csv(dScores,"~/Desktop/psy1903/stats/data_cleaning/data/participant_dScores.csv", row.names = FALSE)
+
+#### (8) Questionnaire Data Extraction --------
+
+## Read in data file to a data frame called iat_test
+# iat_test <- read.csv("~/Desktop/psy1903/stats/data_cleaning/data/my-iat-test-data.csv")
+
+## Extract questionnaire data
+# json_data <- iat_test[iat_test$trialType == "Questionnaire", "response"]
+# 
+# ## Use fromJSON to Convert from JSON to data frame
+# questionnaire <- fromJSON(json_data)
+# questionnaire <- as.data.frame(questionnaire)
+# 
+# ## Convert to numeric
+# questionnaire <- as.data.frame(lapply(questionnaire, as.numeric))
+# 
+# 
+# ## Reverse score if necessary
+
+# ## Calculate mean or sum score
+# score <- rowMeans(questionnaire, na.rm = TRUE)
+
+
+#### (9) Questionnaire Scoring Function ---------
+## Initiate function called score_questionnaire that accepts a single argument called `data`. Within this function...
+
+score_questionnaire <- function (data) {
+  ## Extract questionnaire data cell
+  json_data <- data[data$trialType == "questionnairePartA", "response"]
+  ## Use fromJSON to convert from JSON to data frame
+  questionnaire <- fromJSON(json_data)
+  questionnaire <- as.data.frame(questionnaire)
+  ## Convert to numeric
+  questionnaire <- as.data.frame(lapply(questionnaire, as.numeric))
+  print(questionnaire)
+  ## Reverse score if necessary
+  rev_items <- c("Q0", "Q4", "Q5", "Q9")
+  for (rev_item in rev_items) {
+    questionnaire[, rev_item] <- 5 - questionnaire[, rev_item]
+  }
+  print(questionnaire)
+  ## Calculate & return questionnaire score (mean)
+  score <- rowMeans(questionnaire, na.rm = TRUE)
+  return(score)
+  }
+
+
+#### (10) 
+
+
 
 
